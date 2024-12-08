fa5a648ed124a136095c597d30ef074e6a6aa086
Himanshu Saini <himanshusaini0612@gmail.com>
2024-12-08 11:47:10 +0530

    Code Updated

--- src/components/Headers/Headers.js
+++ src/components/Headers/Headers.js
@@ -8,10 +8,10 @@ const Headers = () => {
   return (<>
     <Navbar bg="dark" data-bs-theme="dark">
         <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Navbar.Brand href="#">MERN STACK</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">About</Nav.Link>
+            <Nav.Link href="#">Home</Nav.Link>
+            <Nav.Link href="#">About</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
